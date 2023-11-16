bdefd2a4ac00bb9f3b5f05e13e05b1ce0644c822
ASLawan <nawalnitsua@gmail.com>
2023-11-16 23:48:32 +0100

    Write a function that initializes struct members

---
+++ set_input_defaults.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+/**
+ * set_input_defaults - initialize input variables
+ * Return: nothing
+ */
+void set_input_defaults(void)
+{
+	input = (inputs *)malloc(sizeof(inputs));
+	if (input == NULL)
+	{
+		not_allocated();
+	}
+	input->instruction = (instruction_t *)malloc(sizeof(instruction_t));
+	if (input->instruction == NULL)
+	{
+		not_allocated();
+	}
+
+	input->source = NULL;
+	input->line = NULL;
+	input->num_tkns = 0;
+	input->line_num = 0;
+	input->height = 0;
+	input->top = NULL;
+}
+
+
