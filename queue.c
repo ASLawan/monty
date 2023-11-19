ea62ba8880d2d141034b14494e5b552fb80f98eb
ASLawan <nawalnitsua@gmail.com>
2023-11-19 07:45:51 +0100

    Define queue data structure

---
+++ queue.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+ * queue - sets data structure to queue
+ * @stack: pointer to stack
+ * @line_number: instruction
+ * Return: nothing
+ */
+
+void queue(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+
+	input->data = 0;
+}
