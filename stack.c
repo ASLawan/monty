c4ad9285857c3084cd520497ad4fdb2a1a251879
ASLawan <nawalnitsua@gmail.com>
2023-11-19 07:46:11 +0100

    Define stack data structure

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+ * stack - sets data structure to stack
+ * @stack: pointer to stack
+ * @line_number: instruction
+ * Return: nothing
+ */
+
+void stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+
+	input->data = 1;
+}
